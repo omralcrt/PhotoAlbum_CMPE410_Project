6abd3d0f75b71634f3a8c0ed4b703a1b2e54c807
Ömral Cörüt <omralcrt@gmail.com>
2017-04-20 13:33:05 +0300

    Initial commit

---
+++ PhotoAlbum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoAlbum
+//
+//  Created by Ömral Cörüt on 19.04.2017.
+//  Copyright © 2017 Ömral Cörüt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
